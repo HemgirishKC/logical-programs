2c2926c7d7ac013d3212bf6e4aa5a0e199d25701
Hemgirish <hemgirishkc@gmail.com>
2024-09-11 22:24:19 +0530

    mulitplication program

---
+++ main2.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class main2
+{
+    public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println("the multiplication of two numbers is "+(a*b));
+    }
+}
